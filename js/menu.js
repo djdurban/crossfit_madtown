75056228a9d1efd41c3bd93bd6db946ab611df35
Abigail Larner <aalarner@gmail.com>
2015-12-31 17:56:40 -0500

    added off canvas menu and script to create new page links

--- js/menu.js
+++ js/menu.js
@@ -1,40 +1,47 @@
-(function() {
-	var triggerBttn = document.getElementById( 'trigger-overlay' ),
-		overlay = document.querySelector( 'div.overlay' ),
-		closeBttn = overlay.querySelector( 'button.overlay-close' );
-		transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		support = { transitions : Modernizr.csstransitions };
 
-	function toggleOverlay() {
-		if( classie.has( overlay, 'open' ) ) {
-			classie.remove( overlay, 'open' );
-			classie.add( overlay, 'close' );
-			var onEndTransitionFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.propertyName !== 'visibility' ) return;
-					this.removeEventListener( transEndEventName, onEndTransitionFn );
-				}
-				classie.remove( overlay, 'close' );
-			};
-			if( support.transitions ) {
-				overlay.addEventListener( transEndEventName, onEndTransitionFn );
-			}
-			else {
-				onEndTransitionFn();
-			}
-		}
-		else if( !classie.has( overlay, 'close' ) ) {
-			classie.add( overlay, 'open' );
-		}
+
+/*====================================
+=            ON DOM READY            =
+====================================*/
+$(function() {
+
+	// Toggle Nav on Click
+	$('.toggle-nav').click(function() {
+		// Calling a function in case you want to expand upon this.
+		toggleNav();
+
+		// if ($('#site-wrapper').hasClass('show-nav')){
+		// 	$(window).click(function() {
+		// 		$('#site-wrapper').removeClass('show-nav');
+		// 	});
+		// }
+
+
+	});
+
+	// $('#site-canvas').click(function() {
+	// 	// Calling a function in case you want to expand upon this.
+	// 	toggleNav();
+
+	// });
+
+
+
+
+});
+
+
+/*========================================
+=            CUSTOM FUNCTIONS            =
+========================================*/
+function toggleNav() {
+	if ($('#site-wrapper').hasClass('show-nav')) {
+
+		$('#site-wrapper').removeClass('show-nav');
+	} else {
+		// Do things on Nav Open
+		$('#site-wrapper').addClass('show-nav');
 	}
 
-	triggerBttn.addEventListener( 'click', toggleOverlay );
-	closeBttn.addEventListener( 'click', toggleOverlay );
-})();
+	//$('#site-wrapper').toggleClass('show-nav');
+}
