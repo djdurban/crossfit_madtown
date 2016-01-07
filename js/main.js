b8ecbef88a2a2784451f75b0eb5fe985a2392a58
Abigail Larner <aalarner@gmail.com>
2016-01-06 22:43:21 -0600

    added img float

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ var pathname = window.location.pathname;
 if(pathname === '/getting-started/2016-01-03-getting-started.html'){
 	// $('#contact p img, #contact h3 + p').css('float', 'left');
 	// $('#contact p').css('float', 'right');
+	$('#contact h3 + p').css('float', 'left');
 }
