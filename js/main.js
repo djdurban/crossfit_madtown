ce2f97e0574abf157e84f65e5aef24572d519d85
Abigail Larner <aalarner@gmail.com>
2016-01-06 22:57:34 -0600

    added more styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-if(window.location.pathname === '/_getting-started/2016-01-03-getting-started.markdown'){
+if (window.location.pathname === '/_getting-started/2016-01-03-getting-started.markdown') {
 	$('p').css('float', 'left');
 	$('p').css('width', '73%');
 	$('p').css('padding-left', '2%');
+	$('h3 + p').css('width', '25%');
+	$('h3 + p').css('float', 'left');
+
 }
