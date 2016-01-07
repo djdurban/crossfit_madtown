1a2917da2ffb451934392c1c5bbadad81792a351
Abigail Larner <aalarner@gmail.com>
2016-01-06 22:40:37 -0600

    fixing specificty

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var pathname = window.location.pathname;
 if(pathname === '/getting-started/2016-01-03-getting-started.html'){
-	$('#contact p img').css('float', 'left');
+	$('#contact p img, #contact h3 + p').css('float', 'left');
 	$('#contact p').css('float', 'right');
 }
