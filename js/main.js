e886398166439150e1563ee8b61f57125c79fbac
Abigail Larner <aalarner@gmail.com>
2016-01-06 22:44:38 -0600

    added more styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var pathname = window.location.pathname;
-if(pathname === '/getting-started/2016-01-03-getting-started.html'){
-	// $('#contact p img, #contact h3 + p').css('float', 'left');
-	// $('#contact p').css('float', 'right');
+if (pathname === '/getting-started/2016-01-03-getting-started.html') {
+	$('#contact h3').css('clear', 'both');
 	$('#contact h3 + p').css('float', 'left');
 }
