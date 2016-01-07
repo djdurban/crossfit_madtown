e4e91ae3e362664c7ab5843f31a6ab6039423e4a
Abigail Larner <aalarner@gmail.com>
2016-01-06 23:02:22 -0600

    js test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-if (window.location.pathname === '/getting-started/2016-01-03-getting-started.html') {
+var pathname = window.location.pathname;
+
+if (pathname === '/getting-started/2016-01-03-getting-started.html') {
 	$('#contact p').css('float', 'left');
 	$('#contact p').css('width', '73%');
 	$('#contact p').css('padding-left', '2%');
 	$('#contact h3 + p').css('width', '25%');
 	$('#contact h3 + p').css('float', 'left');
 	console.log('pathname');
-
+	
 }
