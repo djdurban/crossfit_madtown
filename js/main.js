0d07dc2b73a3a9dcf438c307e814568e83682ebd
Abigail Larner <aalarner@gmail.com>
2016-01-06 23:10:14 -0600

    cleared h3's

--- js/main.js
+++ js/main.js
@@ -7,4 +7,5 @@ if (pathname === '/getting-started/2016-01-03-getting-started.html') {
 	$('#contact h3 + p').css('width', '25%');
 	$('#contact h3 + p').css('float', 'left');
 	$('#contact h3 + p img').css('width', '100%');
+	$('#contact h3').css('clear', 'both');
 }
