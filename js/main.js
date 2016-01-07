bd76100e3e3695e457a10e8151ac5c78250701cc
Abigail Larner <aalarner@gmail.com>
2016-01-06 22:36:27 -0600

    fixing styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var pathname = window.location.pathname;
 if(pathname === '/getting-started/2016-01-03-getting-started.html'){
 	$('#contact img').css('float', 'left');
-	$('#contact p').css('float', 'right');
+	$('#contact p, #contact h3').css('float', 'right');
 }
