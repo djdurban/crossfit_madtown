0a9ed2e77dd49720f6ec82b9b9d7362ad3e72ffd
Abigail Larner <aalarner@gmail.com>
2016-01-06 22:38:41 -0600

    specified img

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var pathname = window.location.pathname;
 if(pathname === '/getting-started/2016-01-03-getting-started.html'){
-	$('#contact img').css('float', 'left');
+	$('#contact p img').css('float', 'left');
 	$('#contact p').css('float', 'right');
 }
